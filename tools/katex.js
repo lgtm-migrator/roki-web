a1328ae1e48d542233b76c929faf86c0ec957f71
roki <falgon53@yahoo.co.jp>
2020-08-13 01:36:45 +0900

    Fix code style: "Strings must use doublequote." and some other styles

--- tools/katex.js
+++ tools/katex.js
@@ -1,6 +1,12 @@
-const fs = require('fs');
-const katex = require('katex');
+const fs = require("fs");
+const katex = require("katex");
 
-const input = fs.readFileSync(process.stdin.fd, 'utf-8');
-const displayMode = process.argv.includes('displayMode');
-process.stdout.write(katex.renderToString(input, { displayMode: displayMode, trust: true, colorIsTextColor: true }));
+const input = fs.readFileSync(process.stdin.fd, "utf-8");
+const displayMode = process.argv.includes("displayMode");
+const mathjaxOpt = {
+    displayMode: displayMode,
+    trust: true,
+    colorIsTextColor: true
+};
+
+process.stdout.write(katex.renderToString(input, mathjaxOpt));
