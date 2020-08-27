dcc9d0880245f08fd47706055f07c20d35b99fe2
roki <falgon53@yahoo.co.jp>
2020-08-27 12:57:33 +0900

    small fix about margin of haskell-jp logo

--- app/site/Main.hs
+++ app/site/Main.hs
@@ -122,6 +122,7 @@ haskellJp = TL.unpack $ renderText $ do
     a_ [href_ "https://haskell.jp/blog/posts/links.html#roki.dev/roki.log/"] $
         img_ [
             width_ "234"
+          , class_ "mt-1"
           , src_ "https://haskell.jp/img/supported-by-haskell-jp.svg"
           , alt_"Supported By Haskell-jp."
           ]
