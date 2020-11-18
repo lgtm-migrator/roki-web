f1ad259de131779db2e6e8755eff682e3b93da52
roki <falgon53@yahoo.co.jp>
2020-11-18 22:11:28 +0900

    refactoring spa

--- app/spa/Main.hs
+++ app/spa/Main.hs
@@ -1,24 +1,26 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main (main) where
 
-import           Config              (hakyllConfig)
-import           Config.Site         (timeZoneJST)
-import           Control.Applicative (optional)
-import           Control.Monad       (unless, (>=>))
-import           Control.Monad.Extra (unlessM)
-import           Data.String         (fromString)
-import           Data.Time           (UTCTime (..), ZonedTime (..),
-                                      defaultTimeLocale, parseTimeM,
-                                      zonedTimeToUTC)
-import           Data.Time.Format    (formatTime)
-import           Data.Tuple.Extra    (dupe, first, second)
-import           Data.Version        (showVersion)
-import           Development.GitRev  (gitBranch, gitHash)
-import qualified Hakyll              as H
-import qualified Options.Applicative as OA
-import qualified Paths_roki_web      as P
-import           System.Exit         (exitFailure, exitSuccess)
-import           System.IO           (hFlush, hPutStrLn, stderr, stdout)
+import           Config                    (hakyllConfig)
+import           Config.Site               (timeZoneJST)
+import           Control.Applicative       (optional)
+import           Control.Monad             (MonadPlus (..), unless, (>=>))
+import           Control.Monad.Extra       (unlessM)
+import           Control.Monad.Trans       (lift)
+import           Control.Monad.Trans.Maybe (MaybeT (..))
+import           Data.String               (fromString)
+import           Data.Time                 (UTCTime (..), ZonedTime (..),
+                                            defaultTimeLocale, parseTimeM,
+                                            zonedTimeToUTC)
+import           Data.Time.Format          (formatTime)
+import           Data.Tuple.Extra          (dupe, first, second)
+import           Data.Version              (showVersion)
+import           Development.GitRev        (gitBranch, gitHash)
+import qualified Hakyll                    as H
+import qualified Options.Applicative       as OA
+import qualified Paths_roki_web            as P
+import           System.Exit               (exitFailure, exitSuccess)
+import           System.IO                 (hFlush, hPutStrLn, stderr, stdout)
 
 putStrLnErrWithExit :: String -> IO ()
 putStrLnErrWithExit = hPutStrLn stderr >=> const exitFailure
@@ -55,42 +57,48 @@ showCexpr :: Opts -> IO ()
 showCexpr opts = maybe (putStrLnErrWithExit "must be specified a valid date string") putStrLn $
     cronize <$> (parseJSTTime =<< optSchedulingDate opts)
 
-genYaml' :: H.Command -> Opts -> IO ()
-genYaml' hcmd opts = H.hakyllWithArgs hakyllConfig' (H.Options False hcmd) $ let utc = parseJSTTime =<< optSchedulingDate opts in
-    case (cronize <$> utc, humanize <$> utc, optBranchName opts) of
-        (Just cExpr, Just date, Just bName) -> do
-            H.create [H.fromFilePath $ bName <> ".yml"] $ do
-                let ctx = H.constField "cron-expr" cExpr
-                        <> H.constField "date" date
-                        <> H.constField "branch-name" bName
-                H.route H.idRoute
-                H.compile $ H.makeItem ""
-                    >>= H.loadAndApplyTemplate (H.fromFilePath "tools/scheduled_post/template.yml") ctx
-            H.match (fromString $ "tools/scheduled_post/**") $ H.compile H.templateBodyCompiler
-        _ -> return ()
+execHakyllFromCmd :: H.Command -> H.Rules a -> IO ()
+execHakyllFromCmd = H.hakyllWithArgs hakyllConfig' . H.Options False
 
-genYaml :: H.Command -> Opts -> IO ()
-genYaml H.Build opts = do
-    unless (optIsForceYes opts) $ do
-        putStrLn $ "current branch name is: " <> $(gitBranch)
-        putStr "are you sure you want to continue connecting? (y/N):" >> hFlush stdout
-        unlessM (uncurry (||) . first (=='y') . second (=='Y') . dupe <$> getChar) $ putStrLn "Canceled" >> exitSuccess
-    genYaml' H.Build opts
-genYaml hcmd opts = genYaml' hcmd opts
+execYaml' :: H.Command -> Opts -> MaybeT IO ()
+execYaml' H.Build opts = let utc = parseJSTTime =<< optSchedulingDate opts in do
+        fName <- MaybeT $ pure ((<> ".yml") <$> optBranchName opts)
+        date <- MaybeT $ pure (humanize <$> utc)
+        bName <- MaybeT $ pure (optBranchName opts)
+        cExpr <- MaybeT $ pure (cronize <$> utc)
+        lift $ do
+            unless (optIsForceYes opts) $ do
+                putStrLn $ "current branch name is: " <> $(gitBranch)
+                putStr "are you sure you want to continue connecting? (y/N)" >> hFlush stdout
+                unlessM (uncurry (||) . first (=='y') . second (=='Y') . dupe <$> getChar) $ putStrLn "Canceled" >> exitSuccess
+            execHakyllFromCmd H.Build $ do
+                H.create [H.fromFilePath fName] $ do
+                    let ctx = H.constField "cron-expr" cExpr
+                            <> H.constField "date" date
+                            <> H.constField "branch-name" bName
+                    H.route H.idRoute
+                    H.compile $ H.makeItem ""
+                        >>= H.loadAndApplyTemplate (H.fromFilePath "tools/scheduled_post/template.yml") ctx
+                H.match (fromString "tools/scheduled_post/**") $ H.compile H.templateBodyCompiler
+execYaml' H.Clean _ = lift $ execHakyllFromCmd H.Clean $ return ()
+execYaml' _ _ = mzero
+
+execYaml :: H.Command -> Opts -> IO ()
+execYaml hcmd opts = maybe (putStrLnErrWithExit "must be specified date and branch") return =<< runMaybeT (execYaml' hcmd opts)
 
 cronExprCmd :: OA.Mod OA.CommandFields Command
 cronExprCmd = OA.command "cexpr" $
-    OA.info (pure $ CmdCronExpr $ showCexpr) $ OA.progDesc "show crontab expression"
+    OA.info (pure $ CmdCronExpr showCexpr) $ OA.progDesc "show crontab expression"
 
 genYamlCmd :: OA.Mod OA.CommandFields Command
 genYamlCmd = OA.command "yaml" $
-    OA.info (pure $ CmdGenYaml $ genYaml H.Build) $
+    OA.info (pure $ CmdGenYaml $ execYaml H.Build) $
         OA.progDesc "generate GitHub Actions yaml from template"
 
 cleanCmd :: OA.Mod OA.CommandFields Command
 cleanCmd = OA.command "clean" $
-    OA.info (pure $ CmdClean $ genYaml H.Clean) $
-        OA.progDesc "Clean up and remove cache"
+    OA.info (pure $ CmdClean $ execYaml H.Clean) $
+        OA.progDesc "clean up and remove cache"
 
 schedulingDate :: OA.Parser (Maybe String)
 schedulingDate = optional $ OA.strOption $ mconcat [
